b978dbe0d206551591a35fc99422b473698efdbe
Robubabu <robert.appel@udo.edu>
2017-11-04 18:21:46 +0100

    Auswertung soweit fertig

--- YRPraktikums-Faraday46/plots/GaAs.py
+++ YRPraktikums-Faraday46/plots/GaAs.py
@@ -6,10 +6,10 @@ from scipy.optimize import curve_fit
 from uncertainties import correlated_values, correlation_matrix
 from uncertainties import ufloat
 from uncertainties.unumpy import (nominal_values as noms, std_devs as stds)
-x = np.linspace(0, 10, 1000)
-mhub = const.value('Bohr magneton') #das gelibete Borhsche Magneton zeigt wie man Scipy Constants benutzt
+x = np.linspace(0, 3, 1000)
+me= const.electron_mass
 n=3.3543 # Brechungsindex für GaAs bei einer Wellenlänge von 1771.14  
-A= (const.e**3 * 0.43) / ( 8*(np.pi**2) * const.epsilon_0*(const.c**3) *n)  #konst zum bestimmen der eff Masse 
+A= (const.e**3 *ufloat(0.457,0.018))  / ( 8*(np.pi**2) * const.epsilon_0*(const.c**3) *n)  #konst zum bestimmen der eff Masse 
 def mittel(x):              #the real mean()-ing of life
     return ufloat(np.mean(x),np.std(x,ddof=1)/np.sqrt(len(x)))
 def relf(l,m):  #in Prozent
@@ -43,45 +43,52 @@ ht = abs(ht)
 t1 = abs(t1)
 t2 = abs(t2)
 #Umrechnen der Wellenlängen
-hl*=10**-6
-l1*=10**-6
-l2*=10**-6
+#hl*=10**-6
+#l1*=10**-6
+#l2*=10**-6
 #Differenzzwischen der hochreinen und den zwei Proben
 D1 = abs(ht/hd-t1/d1) 
 D2 = abs(ht/hd-t2/d2)
-D = np.array([D1[0:2]])
-D = np.append(D,D1[4:])
-L = np.array([hl[0:2]])
-L = np.append(L,hl[4:])
+#D = np.array([D1[0:1]])
+#D = np.append(D,D1[2:])
+#L = np.array([hl[0:1]])
+#L = np.append(L,hl[2:])
 print('Mittelwert der Wellenlängen:' , np.mean(hl))
 #Fit
 C1= A*2.8*10**16 #weil umgerechnet auf Meter 
 C2= A*1.2*10**16
-def fitf1(x,a,b):
-	return x*a + b
-def fitf2(l,m,b):
-	return C2*(1/m)**2 * l**2 +b 
-params , cov = curve_fit(fitf1 ,hl**2,D2)
+def fitf1(x,a):
+	return x*a
+def fitf2(x,b):
+	return x*b 
+params , cov = curve_fit(fitf1 ,hl**2,D1)
 params = correlated_values(params, cov)
-#params2 , cov2 = curve_fit(fitf2 ,hl,D2 )
-#params2 = correlated_values(params2, cov2)
-for p in params:
-	print(p)  
-a =  params[0]
-b = params[1]
+params2 , cov2 = curve_fit(fitf2 ,hl,D2 )
+params2 = correlated_values(params2, cov2)
+a = params[0]
+b = params2[0]
+print(a*10**12,b*10**12)
+print('Hildegard1:', unp.sqrt(C1/(a*10**12)))
+print('Hildegard2:', unp.sqrt(C2/(b*10**12)))
+print('Hildegard1/me:', unp.sqrt((C1/(a*10**12)))/me)
+print('Hildegard2/me:', unp.sqrt((C2/(b*10**12)))/me)
 
 #Tabelle
-# np.savetxt('tab.txt',np.column_stack([x,y]), delimiter=' & ',newline= r'\\'+'\n' )
-plt.plot(L**2 , D, 'ro', label= 'Differenz mit dem 1.n-dotiertem')
-#plt.plot(hl**2,fitf1(hl**2,noms(a)),'b--')
-plt.plot(hl**2, D2, 'bo', label='Differenz zwischen dem 2.n-dotiertem')
-plt.xlabel(r'Wellenlänge zum Quadrat $\lambda^2 \:/\: m^2 $')
-plt.ylabel(r'$\frac{\Delta\theta}{L} / \degree m^{-1} $')
-#plt.legend(loc='best')
-plt.show()
-#plt.savefig('DeltaTheta.pdf')
+np.savetxt('HGaAstab.txt',np.column_stack([(hl*10**(-6)),ht,(ht/hd)]), delimiter=' & ',newline= r'\\'+'\n' )
+np.savetxt('1.nGaAstab.txt',np.column_stack([(l1*10**(-6)),t1,(t1/d1)]), delimiter=' & ',newline= r'\\'+'\n' )
+np.savetxt('2.nGaAstab.txt',np.column_stack([(l2*10**(-6)),t2,(t2/d2)]), delimiter=' & ',newline= r'\\'+'\n' )
+
+plt.plot(hl**2 , D1, 'ro', label= 'Differenz mit 2.8.n-dot')
+plt.plot(x**2,fitf1(x**2,noms(a)),'r--', label = 'Ausgleichsgerade für 2.8 n-dotiert')
+plt.plot(x**2,fitf2(x**2,noms(b)),'b--', label = 'Ausgleichsgerade für 1.2 n-dotiert')
+plt.plot(hl**2, D2, 'bo', label='Differenz mit 1.2.n-dot')
+plt.xlabel(r'Wellenlänge zum Quadrat $\lambda^2 \:/\:\mu m^2 $')
+plt.ylabel(r'Differenz der Längennormierten Winkel $\frac{\Delta\theta}{L} / \degree m^{-1} $')
+plt.legend(loc='best')
+#plt.show()
+plt.savefig('DeltaTheta.pdf')
 #plt.title('Farady-Rotation der GaAs-Proben im Vergleich')
-#plt.plot((l2**2),t2/d2,'go',label='n-dotiertes GaAs mit $N=1,2\cdot10^{18}cm^{-3}$')
+#plt.plot((l2**2),t2/d2,'go',label='n-dotiertes GaAs mit $N=1,2\cdot10^{18}cm^{-3}FF$')
 #plt.plot((l1**2),t1/d1,'bo',label=r'n-dotiertes GaAs mit $ N=2,8\cdot10^{18}cm^{-3}$')
 #plt.plot((hl**2),ht/hd,'ro', label=r'Hochreines GaAs')
 #plt.xlabel(r'Wellenlänge zum Quadrat $\lambda^2 \:/\: m^2 $')
@@ -89,35 +96,3 @@ plt.show()
 #plt.legend(loc='best')
 #plt.savefig('GaAsimVgl.pdf')
 #plt.show()
-
-
-
-#plt.title('Hochreines GaAs')
-#plt.plot(hl**2,ht,'ro', label=r'Fraday-Rotation gg. Wellenlänge')
-#plt.xlabel(r'$\lambda \:/\:\mu m $')
-#plt.ylabel(r'$\Delta\theta \:/\:\degree $')
-#plt.legend(loc='best')
-#plt.savefig('hochreinesGaAsplot.pdf')
-#plt.show()
-
-#plt.clf()
-
-#plt.title(r'n-dotiertes GaAs mit $ N=2,8\cdot10^{18}cm^{-3}$')
-#plt.plot(l1**2,t1,'bo',label=r'Fraday-Rotation gg. Wellenlänge')
-#plt.xlabel(r'$\lambda \:/\:\mu m$')
-#plt.ylabel(r'$\Delta\theta \:/\:\degree $')
-#plt.legend(loc=r'best')
-#plt.savefig('1.nGaAsplot.pdf')
-#plt.show()
-
-#plt.clf()
-
-#plt.title('n-dotiertes GaAs mit $N=1,2\cdot10^{18}cm^{-3}$')
-#plt.plot(l2**2,t2,'go',label='Fradday Rotation gg. Wellenlänge')
-#plt.xlabel(r'$\lambda \:/\:\mu m $')
-#plt.ylabel(r'$\Delta\theta \:/\:\degree $')
-#plt.legend(loc='best')
-#plt.savefig('2.nGaAsplot.pdf')
-#plt.show()
-
-
